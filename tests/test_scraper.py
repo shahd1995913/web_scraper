6ec8d717205548acb9b83763dc9c8149a3ad30ff
Shahed  Nabeel Alkhateeb <83476666+shahd1995913@users.noreply.github.com>
2021-11-07 19:11:29 +0400

    Update test_scraper.py

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,5 +1,30 @@
-from web_scraper import __version__
+from web_scraper.scraper import (get_citations_needed_count , get_citations_needed_report)
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+
+def test_get_citations_needed_count():
+
+    link ='https://en.wikipedia.org'
+
+    link_contain = f'{link}//wiki/History_of_Mexico'
+
+
+    excepted = 5
+
+    actual = get_citations_needed_count(link_contain)
+
+
+    assert excepted == actual
+    
+    
+def test_get_citations_needed_report():
+
+    link ='https://en.wikipedia.org'
+
+    link_contain = f'{link}//wiki/History_of_Mexico'
+
+
+    excepted = get_citations_needed_report(link_contain)
+    
+    assert 'The Spanish had no intention to turn over Tenochtitlan to the Tlaxcalteca.' in excepted
+
